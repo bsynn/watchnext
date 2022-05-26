8556f93ea0c7dcc98c96a8f072966f4cc8ca5ff4
FameFridge <105991717+FameFridge@users.noreply.github.com>
2022-05-26 14:43:53 +0700

    Add files via upload

--- integration_test/robots/create_robot.dart
+++ integration_test/robots/create_robot.dart
@@ -48,6 +48,45 @@ class Createrobot {
     expect(find.text("This email is already in use! Change to another email."),
         findsWidgets);
     await tester.pumpAndSettle(new Duration(seconds: 5));
-    
+  }
+
+  //null tab register
+  Future<void> NullInput({bool scrollUp = false}) async {
+    final Regisbtn = find.byKey(const Key("Regisbtn1"));
+
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.tap(Regisbtn);
+    await tester.pumpAndSettle(new Duration(seconds: 5));
+  }
+
+  Future<void> fillNameOnly({bool scrollUp = false}) async {
+    final Regisbtn = find.byKey(const Key("Regisbtn1"));
+
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.enterText(find.byKey(const Key("Name")), 'god1');
+    await tester.tap(Regisbtn);
+    await tester.pumpAndSettle();
+    expect(find.text("This field is required!"), findsWidgets);
+    await tester.pumpAndSettle(new Duration(seconds: 5));
+  }
+
+  //Still erroe
+  Future<void> WrongFormatEmail({bool scrollUp = false}) async {
+    final Regisbtn = find.byKey(const Key("Regisbtn1"));
+
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.enterText(find.byKey(const Key("Name")), 'god1');
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.enterText(find.byKey(const Key("Last Name")), 'god1');
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.enterText(find.byKey(const Key("Email")), 'god1.com');
+    await tester.pump(Duration(milliseconds: 400));
+    await tester.enterText(find.byKey(const Key("Password")), '123456');
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pumpAndSettle(new Duration(seconds: 2));
+    await tester.tap(Regisbtn);
+    await tester.pumpAndSettle(new Duration(seconds: 5));
+    expect(find.text("Invalid email format!"), findsWidgets);
+    await tester.pumpAndSettle(new Duration(seconds: 5));
   }
 }
